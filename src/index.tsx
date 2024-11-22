b0871112b98891876d2c5e6ba0a964f3b75487d0
Iván Pondal <ivan.pondal@gmail.com>
2024-11-22 11:52:39 -0300

    Add scaffolding

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App: React.FC = () => <h1>Hello, React with TypeScript and Parcel!</h1>;
+
+ReactDOM.render(<App />, document.getElementById("root"));
