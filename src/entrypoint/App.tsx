95b1ae9324c2764fbb271e374e5f611de5c94ea1
Iván Pondal <ivan.pondal@gmail.com>
2024-11-22 12:30:43 -0300

    Remove title

--- src/entrypoint/App.tsx
+++ src/entrypoint/App.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
 import { ViewerCanvas } from '../viewer/ViewerCanvas';
 
-export const App: React.FC = () => (<>
-    <h1>Hello, React with TypeScript and Parcel!</h1>
-    <ViewerCanvas />
-</>)
+export const App: React.FC = () => <ViewerCanvas />
