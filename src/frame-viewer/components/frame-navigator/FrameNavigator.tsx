184fe727048a32c6d49abf60a168a98e11d5a62d
Iván Pondal <ivan.pondal@gmail.com>
2024-11-22 19:12:48 -0300

    Add frame selection

--- src/frame-viewer/components/frame-navigator/FrameNavigator.tsx
+++ src/frame-viewer/components/frame-navigator/FrameNavigator.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAsync } from "react-use";
 import { frameApiClient } from "../../services/frame-api-client";
 import { ViewerCanvas } from "../viewer-canvas/ViewerCanvas";
@@ -6,14 +6,16 @@ import './FrameNavigator.css';
 import { Slider } from "../../../ui/slider/Slider";
 
 export const FrameNavigator = () => {
-    const state = useAsync(async () => frameApiClient.get(0), []);
+    const [selectedFrameId, setSelectedFrameId] = useState(0);
+
+    const state = useAsync(async () => frameApiClient.get(selectedFrameId), [selectedFrameId]);
 
     return <div className="frame-navigator">
         {state.loading && <h1>Loading...</h1> ||
             (!state.loading && state.value !== undefined) && <ViewerCanvas frame={state.value} />}
 
         <div className="frame-navigator__overlay">
-            <Slider />
+            <Slider currentValue={selectedFrameId} onChange={(newValue) => setSelectedFrameId(newValue)} />
         </div>
     </div>
 }
--- src/ui/slider/Slider.tsx
+++ src/ui/slider/Slider.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import './Slider.css';
 
-export const Slider = () => {
-    return <div className="slider"><h1>slider</h1></div>
+type SliderProps = {
+    currentValue: number;
+    onChange: (newValue: number) => void
+}
+
+export const Slider = ({ currentValue, onChange }: SliderProps) => {
+    return <>
+        <input className="slider" type="range"
+            min={0} max={49} value={currentValue} step={1} list="markers"
+            onChange={(event) => onChange(parseInt(event.target.value))} />
+        <datalist id="markers">
+            <option value="0"></option>
+            <option value="25"></option>
+            <option value="50"></option>
+        </datalist>
+    </>
 }
