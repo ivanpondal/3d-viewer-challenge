314903b6806ee91fb2e2a4de1820efcafad04fdc
Iván Pondal <ivan.pondal@gmail.com>
2024-11-22 21:06:44 -0300

    Fix cuboid edges

--- src/frame-viewer/components/geometry/TranslucidCuboid.tsx
+++ src/frame-viewer/components/geometry/TranslucidCuboid.tsx
@@ -1,6 +1,7 @@
 import React, { useMemo, useState } from "react"
 import { Cuboid } from "../../services/frame-api-client"
 import { Vector3 } from "three"
+import { Edges } from "@react-three/drei"
 
 type TranslucidCuboidProps = {
     cuboid: Cuboid,
@@ -13,31 +14,27 @@ export const TranslucidCuboid = ({ cuboid, onPointerEnter, onPointerLeave }: Tra
 
     const position = useMemo(() => new Vector3(cuboid.position.x, cuboid.position.y, cuboid.position.z), [cuboid.position])
 
-    return <>
-        <mesh
-            position={position}
-            rotation={[0, 0, cuboid.yaw]}
-        >
-            <boxGeometry args={[cuboid.dimensions.x, cuboid.dimensions.y, cuboid.dimensions.z]} />
+    return <mesh
+        position={position}
+        rotation={[0, 0, cuboid.yaw]}
+        onPointerEnter={() => {
+            setHovered(true);
+            onPointerEnter?.();
+        }
+        }
+        onPointerLeave={() => {
+            setHovered(false);
+            onPointerLeave?.();
+        }}
+    >
+        <boxGeometry args={[cuboid.dimensions.x, cuboid.dimensions.y, cuboid.dimensions.z]} />
 
-            <meshStandardMaterial color={0xff0000} transparent opacity={hovered ? 0.8 : 0.6} />
-        </mesh>
-        <mesh
-            position={position}
-            rotation={[0, 0, cuboid.yaw]}
-            onPointerEnter={() => {
-                setHovered(true);
-                onPointerEnter?.();
-            }
-            }
-            onPointerLeave={() => {
-                setHovered(false);
-                onPointerLeave?.();
-            }}
-        >
-            <boxGeometry args={[cuboid.dimensions.x, cuboid.dimensions.y, cuboid.dimensions.z]} />
-
-            <meshStandardMaterial color={0x00000} wireframe />
-        </mesh>
-    </>
+        <meshStandardMaterial color={0x00ff00} transparent opacity={hovered ? 0.8 : 0.6} />
+        <Edges
+            linewidth={hovered ? 8 : 4}
+            scale={1}
+            threshold={15}
+            color={0x00ff00}
+        />
+    </mesh>
 }
