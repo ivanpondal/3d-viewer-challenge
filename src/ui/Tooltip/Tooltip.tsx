51a2928ce739a3c76aeccd453e35f2e292cb7b2a
Iván Pondal <ivan.pondal@gmail.com>
2024-11-23 01:20:04 -0300

    Add dimension to tooltip

--- src/ui/Tooltip/Tooltip.tsx
+++ src/ui/Tooltip/Tooltip.tsx
@@ -20,6 +20,7 @@ export const Tooltip = ({ cuboid, onDismiss }: TooltipProps) => {
     return <div className="tooltip" onClick={onDismiss}>
         <Label property="uuid" value={cuboid.uuid} />
         <Label property="position [x, y, z]" value={`[${cuboid.position.x}, ${cuboid.position.y}, ${cuboid.position.z}]`} />
+        <Label property="dimensions [x, y, z]" value={`[${cuboid.dimensions.x}, ${cuboid.dimensions.y}, ${cuboid.dimensions.z}]`} />
         <Label property="label" value={cuboid.label} />
         <Label property="stationary" value={cuboid.stationary ? "yes" : "no"} />
         <Label property="camera used" value={cuboid.cameraUsed.toString()} />
